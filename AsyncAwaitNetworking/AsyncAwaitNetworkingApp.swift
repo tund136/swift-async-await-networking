027b11f2e6b7f1097570fa736c014a05114da5a3
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 11:28:02 +0700

    Create the project

---
+++ AsyncAwaitNetworking/AsyncAwaitNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitNetworkingApp.swift
+//  AsyncAwaitNetworking
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitNetworking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncAwaitNetworking
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
