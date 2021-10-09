f7c7eaa00d571040c6eed385136c48172dd5b913
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 16:03:47 +0700

    Setting up Character Service

---
+++ AsyncAwaitNetworking/Service/CharacterService.swift
@@ -0,0 +1,41 @@
+//
+//  CharacterService.swift
+//  AsyncAwaitNetworking
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import Foundation
+
+struct CharacterService {
+    enum CharacterServiceError: Error {
+        case failed
+        case failedToDecode
+        case invalidStatusCode
+    }
+    
+    func fetchCharacters(completion: @escaping (Result<[Character], Error>) -> Void) {
+        let session = URLSession.shared.dataTask(with: .init(url: URL(string: "https://rickandmortyapi.com/api/character")!)) { data, response, error in
+            guard let response = response as? HTTPURLResponse
+            else {
+                return
+            }
+            
+            guard response.statusCode == 200
+            else {
+                completion(.failure(CharacterServiceError.invalidStatusCode))
+                return
+            }
+            
+            guard let data = data, let decodedData = try? JSONDecoder().decode(CharacterServiceResult.self, from: data)
+            else {
+                completion(.failure(CharacterServiceError.failedToDecode))
+                return
+            }
+            
+            completion(.success(decodedData.results))
+        }
+        
+        session.resume()
+    }
+}
