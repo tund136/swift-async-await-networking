5cc6d865541fbfbe4b570c6c49f856473237aee6
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 11:13:38 +0700

    Comment code and update ios 15

--- AsyncAwaitNetworking/Service/CharacterService.swift
+++ AsyncAwaitNetworking/Service/CharacterService.swift
@@ -14,28 +14,29 @@ struct CharacterService {
         case invalidStatusCode
     }
     
-    func fetchCharacters(completion: @escaping (Result<[Character], Error>) -> Void) {
-        let session = URLSession.shared.dataTask(with: .init(url: URL(string: "https://rickandmortyapi.com/api/character")!)) { data, response, error in
-            guard let response = response as? HTTPURLResponse
-            else {
-                return
-            }
-            
-            guard response.statusCode == 200
-            else {
-                completion(.failure(CharacterServiceError.invalidStatusCode))
-                return
-            }
-            
-            guard let data = data, let decodedData = try? JSONDecoder().decode(CharacterServiceResult.self, from: data)
-            else {
-                completion(.failure(CharacterServiceError.failedToDecode))
-                return
-            }
-            
-            completion(.success(decodedData.results))
-        }
-        
-        session.resume()
-    }
+    // Breaking down the benefits of using Swift Concurrency's Async/Await
+    //    func fetchCharacters(completion: @escaping (Result<[Character], Error>) -> Void) {
+    //        let session = URLSession.shared.dataTask(with: .init(url: URL(string: "https://rickandmortyapi.com/api/character")!)) { data, response, error in
+    //            guard let response = response as? HTTPURLResponse
+    //            else {
+    //                return
+    //            }
+    //
+    //            guard response.statusCode == 200
+    //            else {
+    //                completion(.failure(CharacterServiceError.invalidStatusCode))
+    //                return
+    //            }
+    //
+    //            guard let data = data, let decodedData = try? JSONDecoder().decode(CharacterServiceResult.self, from: data)
+    //            else {
+    //                completion(.failure(CharacterServiceError.failedToDecode))
+    //                return
+    //            }
+    //
+    //            completion(.success(decodedData.results))
+    //        }
+    //
+    //        session.resume()
+    //    }
 }
