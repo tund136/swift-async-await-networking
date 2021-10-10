7de8c731b37fbe3f11aa5d37c5aed91fa4e12f47
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 17:00:24 +0700

    Testing out

--- AsyncAwaitNetworking/Service/CharacterService.swift
+++ AsyncAwaitNetworking/Service/CharacterService.swift
@@ -42,7 +42,10 @@ struct CharacterService {
     
     func fetchCharacters() async throws -> [Character] {
         let url = URL(string: "https://rickandmortyapi.com/api/character")!
-        let (data, response) = try await URLSession.shared.data(from: url)
+        let configuration = URLSessionConfiguration.ephemeral
+        
+        // let (data, response) = try await URLSession.shared.data(from: url)
+        let (data, response) = try await URLSession(configuration: configuration).data(from: url)
         
         guard let response = response as? HTTPURLResponse, response.statusCode == 200
         else {
