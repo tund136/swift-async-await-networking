0e3b9c315830ba3abeb27d89c55a1e01a3849b7a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-10 11:32:23 +0700

    Using Async/Await in Networking

--- AsyncAwaitNetworking/Service/CharacterService.swift
+++ AsyncAwaitNetworking/Service/CharacterService.swift
@@ -39,4 +39,18 @@ struct CharacterService {
     //
     //        session.resume()
     //    }
+    
+    func fetchCharacters() async throws -> [Character] {
+        let url = URL(string: "https://rickandmortyapi.com/api/character")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200
+        else {
+            throw CharacterServiceError.invalidStatusCode
+        }
+        
+        let decodedData = try JSONDecoder().decode(CharacterServiceResult.self, from: data)
+        
+        return decodedData.results
+    }
 }
